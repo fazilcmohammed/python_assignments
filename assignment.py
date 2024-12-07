f7300ddf1cc3d1c6820c4b95c48847d4d318c89e
fazilcmohammed <crackthedata@gmail.com>
2024-12-07 09:57:05 +0530

    Initial commit

---
+++ assignment.py
@@ -0,0 +1,27 @@
+# 1. Accept two inputs from the user and output its sum.
+
+# num1 = int(input('Enter a number: '))
+# num2 = int(input('Enter another number: '))
+# sum = num1 + num2
+# print(f"the sum of your numbers is {sum}")
+
+# 2. Write a program to find the simple interest.
+# a. Program should accept 3 inputs from the user and calculate simple interest for the given inputs.
+#     Formula: SI=(P*R*n)/100)
+
+# p = int(input('Enter the amount : '))
+# r = int(input('Enter the interest percentage per annum: '))
+# t = int(input('How long is being invested (by years): '))
+# si = int((p*r*t)/100)
+# print(f"The Simple Interest for your amount is {si}")
+
+# 3.  Write a program to check whether a student has passed or failed in a subject after he or she enters
+#     their mark (pass mark for a subject is 50 out of 100).
+#     b. Program should accept an input from the user and output a message as “Passed” or “Failed”
+
+mark = int(input('Pls enter your score: '))
+
+if mark >= 50:
+    print("Congrates, you passed!")
+else:
+    print('Sorry, you failed.')
