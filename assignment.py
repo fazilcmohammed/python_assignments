5a292be12f9ffa8cfc90582a02fcbb8ef08dae42
fazilcmohammed <crackthedata@gmail.com>
2024-12-07 17:58:13 +0530

    solved problems 5, 6 and 7

--- assignment.py
+++ assignment.py
@@ -30,18 +30,71 @@
 #     percentage.
 #     b. Program should accept an input from the user and display their grade as follows
 
-percentage = int(input('Enter your total mark percentage: '))
+# percentage = int(input('Enter your total mark percentage: '))
 
-if percentage >= 90:
-    grade = "A"
-elif percentage >= 80:
-    grade = "B"
-elif percentage >= 70:
-    grade = "C"
-elif percentage >= 60:
-    grade = "D"
-else:
-    grade = "F"
+# if percentage >= 90:
+#     grade = "A"
+# elif percentage >= 80:
+#     grade = "B"
+# elif percentage >= 70:
+#     grade = "C"
+# elif percentage >= 60:
+#     grade = "D"
+# else:
+#     grade = "F"
 
-# Display the grade
-print(f"Your grade is: {grade}")
+# # Display the grade
+# print(f"Your grade is: {grade}")
+
+
+# 5.	Using the ‘switch case’ write a program to accept an input number from the user and output the day as follows. 
+
+# day = int(input('Enter a number: '))
+
+# match day:
+#     case 1:
+#         print('Sunday') 
+#     case 2:
+#         print('Monday') 
+#     case 3:
+#         print('Tuesday')
+#     case 4:
+#         print('Wednesday')
+#     case 5:
+#         print('Thursday')
+#     case 6:
+#         print('Friday')
+#     case 7:
+#         print('Saturday')
+#     case _:
+#         print('Invalid Entry') 
+
+# 6.	Write a program to print the multiplication table of given numbers.
+# a.	Accept an input from the user and display its multiplication table
+
+# num = int(input('Enter a number: '))
+
+# table = range(1, 11)
+
+# for n in table:
+#     product = n * num
+#     print(f"{n}*{num} = {product} ")
+
+
+# 7.	Write a program to find the sum of all the odd numbers for a given limit
+# a.	Program should accept an input as limit from the user and display the sum of all the odd numbers within that limit
+
+limit = int(input('Enter a limit: '))
+
+num = range(1, limit+1)
+odd_num = []
+sum = 0
+
+for n in num:
+    if n%2 !=0:
+        odd_num.append(n)
+        sum += n
+
+print(odd_num)
+print(sum)
+    
